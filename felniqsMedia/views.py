ed404f3c96f958b10bbd1d226239552787cb0193
felniq <felniq@gmail.com>
2020-03-11 01:57:56 +0200

    some functionality added to views

--- felniqsMedia/views.py
+++ felniqsMedia/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import AvailableImages
 
 
 def index(request):
-    return HttpResponse("this is the index of the felniqsMedia")
+    all_images_list = AvailableImages.objects.order_by('-pub_date')[:5]
+    context = {'all_images_list': all_images_list}
+    return render(request, 'felniqsMedia/index.html', context)
